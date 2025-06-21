998f5c29f7be8405cb6c347a479240341092eefd
lianhuadeng <2171822821@qq.com>
2025-06-21 10:31:44 +0800

    项目初始化

---
+++ src/test/java/com/scu/accommodationmanagement/AccommodationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scu.accommodationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
