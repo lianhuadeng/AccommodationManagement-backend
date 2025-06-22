2314f19e4976b0e3cbf30e8cd0ac350eb696d4de
lianhuadeng <2171822821@qq.com>
2025-06-22 11:19:17 +0800

    项目初始化

--- src/main/java/com/scu/accommodationmanagement/controller/StudentController.java
+++ src/main/java/com/scu/accommodationmanagement/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.scu.accommodationmanagement.controller;
 
 
+import com.scu.accommodationmanagement.model.po.Student;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-
+    @PostMapping("add")
+    public void addStudent(@RequestBody Student student) {
+        System.out.println(student);
+    }
 }
