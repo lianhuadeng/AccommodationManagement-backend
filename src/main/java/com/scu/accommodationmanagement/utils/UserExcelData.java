832649d40dbb5091ee6aad24b85d1bc3a4d4742e
lianhuadeng <2171822821@qq.com>
2025-06-26 15:27:47 +0800

    修改excel导入学生方式为按列名导入

--- src/main/java/com/scu/accommodationmanagement/utils/UserExcelData.java
+++ src/main/java/com/scu/accommodationmanagement/utils/UserExcelData.java
@@ -5,51 +5,51 @@ import lombok.Data;
 
 @Data
 public class UserExcelData {
-    @ExcelProperty(index = 0)
-    private Long userId;
-
-    @ExcelProperty(index = 1)
-    private String name;
-
-    @ExcelProperty(index = 2)
-    private String college;
-
-    @ExcelProperty(index = 3)
-    private String major;
-
-    @ExcelProperty(index = 4)
-    private Long grade;
-
-    @ExcelProperty(index = 5)
-    private Long clazz;
-
-    @ExcelProperty(index = 6)
-    private String sex;
-
-    @ExcelProperty(index = 7)
-    private String contact;
-
-//    @ExcelProperty("学号")
+//    @ExcelProperty(index = 0)
 //    private Long userId;
 //
-//    @ExcelProperty("姓名")
+//    @ExcelProperty(index = 1)
 //    private String name;
 //
-//    @ExcelProperty("学院")
+//    @ExcelProperty(index = 2)
 //    private String college;
 //
-//    @ExcelProperty("专业")
+//    @ExcelProperty(index = 3)
 //    private String major;
 //
-//    @ExcelProperty("年级")
+//    @ExcelProperty(index = 4)
 //    private Long grade;
 //
-//    @ExcelProperty("班级")
+//    @ExcelProperty(index = 5)
 //    private Long clazz;
 //
-//    @ExcelProperty("性别")
+//    @ExcelProperty(index = 6)
 //    private String sex;
 //
-//    @ExcelProperty("联系方式")
+//    @ExcelProperty(index = 7)
 //    private String contact;
+
+    @ExcelProperty("学号")
+    private Long userId;
+
+    @ExcelProperty("姓名")
+    private String name;
+
+    @ExcelProperty("学院")
+    private String college;
+
+    @ExcelProperty("专业")
+    private String major;
+
+    @ExcelProperty("年级")
+    private Long grade;
+
+    @ExcelProperty("班级")
+    private Long clazz;
+
+    @ExcelProperty("性别")
+    private String sex;
+
+    @ExcelProperty("联系方式")
+    private String contact;
 }
--- src/test/java/com/scu/accommodationmanagement/AccommodationManagementApplicationTests.java
+++ src/test/java/com/scu/accommodationmanagement/AccommodationManagementApplicationTests.java
@@ -1,5 +1,6 @@
 package com.scu.accommodationmanagement;
 
+import com.scu.accommodationmanagement.utils.Md5Util;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +10,13 @@ class AccommodationManagementApplicationTests {
     @Test
     void uploadTest() {
 
+    }
+
+    @Test
+    void Md5Test() {
+        String defaultPassword = "2022141460307";
+        defaultPassword = defaultPassword.substring(Math.max(0, defaultPassword.length() - 6));
+        System.out.println(Md5Util.getMD5String(defaultPassword));
     }
 
 }
